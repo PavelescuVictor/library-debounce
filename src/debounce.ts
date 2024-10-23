d25ff75aa52a57dd2667b063e464c258b11022b6
Victor Pavelescu <victor.pavelescu@endava.com>
2024-10-23 12:48:13 +0300

    refactor: change logic for handling config values

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,38 +1,104 @@
-import { IDebounceConfig } from './debounce.types';
+import { 
+    IDebounceConfig,
+} from './debounce.types';
 
-export const debounce = (callback: Function, debounceConfig: IDebounceConfig): Function => {
+export const debounce = (callback: Function, config: IDebounceConfig = {}): Function => {
     const {
         debounceTime = 1000,
-        maxWaitCalls = Infinity,
-    } = debounceConfig;
+        maxSkippedCalls = Infinity,
+        maxSkippedTime = Infinity,
+        leading = false,
+        trailing = true,
+    } = config;
 
-    let timeoutId: number | ReturnType<typeof setTimeout> = -1;
-    let currentWaitAmount: number = 0;
+    let _timeoutId: number | ReturnType<typeof setTimeout> = -1;
+    let _skippedCallsCounter: number = 0;
+    let _startTime: number = -1;
+    let _shouldLead: boolean = leading;
 
-    const sanitizeCurrentTimeout = () => {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-            currentWaitAmount++;
+    const resetCurrentTimeout = () => {
+        if (_timeoutId === -1) {
+            return;
+        }
+        clearTimeout(_timeoutId);
+        _skippedCallsCounter++;
+        _timeoutId = -1;
+    }
+
+    const maxSkippedCallsReset = () => {
+        _skippedCallsCounter = 0;
+    }
+
+    const maxSkippedCallsCheck = (handleCallback: () => void) => {
+        if (maxSkippedCalls === Infinity) {
+            return;
+        }
+
+        if (_skippedCallsCounter >= maxSkippedCalls) {
+            handleCallback();
+            maxSkippedCallsReset();
         }
     }
 
-    const maxWaitReset = (...args: any) => {
-        if (currentWaitAmount >= maxWaitCalls) {
-            callback(...args);
-            currentWaitAmount = 0;
+    const maxSkippedtimeReset = (now: number = -1) => {
+        _startTime = now;
+    }
+
+    const maxSkippedTimeCheck = (handleCallback: () => void) => {
+        if (maxSkippedTime === Infinity) {
+            return;
+        }
+
+        const now = Date.now();
+        let elapsedTime = Date.now() - _startTime;
+        if (elapsedTime >= maxSkippedTime) {
+            handleCallback();
+            maxSkippedtimeReset(now);
         }
     }
 
-    const handleNewTimeout = (...args: any) => {
-        timeoutId = setTimeout(() => {
-            callback(...args);
+    const leadingCheck = (leading: boolean, handleCallback: () => void) => {
+        if (!leading || !_shouldLead) {
+            return;
+        }
+
+        handleCallback();
+        _shouldLead = false;
+    }
+
+    const leadingReset = () => {
+        _shouldLead = leading;
+    }
+
+    const trailingCheck = (trailing: boolean, handleCallback: () => void) => {
+        if (!trailing) {
+            return;
+        }
+        handleCallback();
+    }
+
+    const handleNewTimeout = (handleCallback: () => void) => {
+        if (_timeoutId) {
+            resetCurrentTimeout();
+        }
+        _timeoutId = setTimeout(() => {
+            trailingCheck(trailing, handleCallback);
+            maxSkippedCallsReset();
+            maxSkippedtimeReset();
+            leadingReset();
         }, debounceTime)
     }
 
     return (...args: any) => {
-        sanitizeCurrentTimeout();
-        maxWaitReset(...args);
-        handleNewTimeout(...args);
+        const _context = this;
+        const handleCallback = () => {
+            callback.apply(_context, ...args);
+        }
+        resetCurrentTimeout();
+        maxSkippedCallsCheck(handleCallback);
+        maxSkippedTimeCheck(handleCallback);
+        leadingCheck(leading, handleCallback);
+        handleNewTimeout(handleCallback);
     };
 }
 
