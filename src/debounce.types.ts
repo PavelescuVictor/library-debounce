e22ed9174636ef78ccd1b672df7bf994d45273c2
Victor Pavelescu <victor.pavelescu@endava.com>
2024-10-23 16:31:59 +0300

    feat: add types

--- src/debounce.types.ts
+++ src/debounce.types.ts
@@ -5,3 +5,8 @@ export interface IDebounceConfig {
     leading?: boolean,
     trailing?: boolean,
 }
+
+export type Callback = (...args: any[]) => any;
+export type CallbackReturn<T extends Callback> = ReturnType<T> | undefined;
+export type Debounce = <T extends Callback>(callback: T, config?: IDebounceConfig) => (...args: Parameters<T>) => CallbackReturn<T>;
+export type HandleCallback<T extends Callback> = () => CallbackReturn<T>;
